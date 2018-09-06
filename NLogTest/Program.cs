41e08eff180478f356f1d7c1d52911f2d461e3a6
wblong <sdwangbaolong@163.com>
2018-09-06 13:39:49 +0800

    NLOG配置

--- NLogTest/Program.cs
+++ NLogTest/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
+
 
 namespace NLogTest
 {
     class Program
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            WriteLog();
+            LogManager.Flush();
+        }
+        public static void WriteLog()
+        {
+            Logger.Log(LogLevel.Error, "nlog test error");
         }
     }
 }
